b5f87bf13449d1c8f333a8b137d51e345533e227
J. Derksen <jip.derksen@hva.nl>
2023-12-18 12:42:59 +0100

    2023 day-18 done!

--- 2023/day-18/solution.py
+++ 2023/day-18/solution.py
@@ -1,59 +1,67 @@
-# to low: 43624
-# answer: 47675
-
 instructions = []
-grid = [['.' for _ in range(390)] for _ in range(390)]
+colors = []
 
 for line in open('data.txt'):
     instruction, value, color = line.strip().split(' ')
     instructions.append((instruction, int(value)))
+    colors.append(color)
 
-pos = (0, 0)
-path = []
-for instruction in instructions:
-    if instruction[0] == 'R':
-        for i in range(instruction[1]):
-            grid[pos[0]][pos[1]] = '#'
-            path.append(pos)
-            pos = (pos[0], pos[1] + 1)
-    elif instruction[0] == 'L':
-        for i in range(instruction[1]):
-            grid[pos[0]][pos[1]] = '#'
-            path.append(pos)
-            pos = (pos[0], pos[1] - 1)
-    elif instruction[0] == 'U':
-        for i in range(instruction[1]):
-            grid[pos[0]][pos[1]] = '#'
-            path.append(pos)
-            pos = (pos[0] - 1, pos[1])
-    elif instruction[0] == 'D':
-        for i in range(instruction[1]):
-            grid[pos[0]][pos[1]] = '#'
-            path.append(pos)
-            pos = (pos[0] + 1, pos[1])
+def get_path_part1(instructions):
+    x, y = 0, 0
+    path = [(x, y)]
 
-def is_surrounded(pos):
-    found_left, found_right, found_up, found_down = False, False, False, False
-    for i in range(0, len(grid)):
-        if pos[0] - i >= 0 and grid[pos[0] - i][pos[1]] == '#':
-            found_up = True
-        if pos[0] + i < len(grid) and grid[pos[0] + i][pos[1]] == '#':
-            found_down = True
-        if pos[1] - i >= 0 and grid[pos[0]][pos[1] - i] == '#':
-            found_left = True
-        if pos[1] + i < len(grid) and grid[pos[0]][pos[1] + i] == '#':
-            found_right = True
-    return found_left and found_right and found_up and found_down
+    for direction, steps in instructions:
+        if direction == 'R':
+            x += steps
+        elif direction == 'L':
+            x -= steps
+        elif direction == 'U':
+            y += steps
+        elif direction == 'D':
+            y -= steps
+        path.append((x, y))
 
-count = 0
-for i in range(len(grid)):
-    for j in range(len(grid)):
-        if is_surrounded((i, j)):
-            grid[i][j] = '#'
-            count += 1
+    return path
 
-print(sum([sum([1 if x == '#' else 0 for x in row]) for row in grid]))
+def get_path_length(instructions):
+    length = 0
+    for _, steps in instructions:
+        length += steps
+    return length
 
-for line in grid:
-    print(''.join(line))
 
+
+def get_instructions_part2():
+    new_instructions = []
+    for color in colors:
+        dir_map = {'0': 'R', '1': 'D', '2': 'L', '3': 'U'}
+        direction = dir_map[color[-2]]
+        hex_value = int(color[2:-2], 16)
+        new_instructions.append((direction, hex_value))
+    return new_instructions
+
+
+def shoelace_formula(x, y):
+    n = len(x)
+    area = 0.5 * abs(sum(x[i] * y[(i + 1) % n] - x[(i + 1) % n] * y[i] for i in range(n)))
+    return area
+
+def calculate_surface_area(outline):
+    x_coordinates, y_coordinates = zip(*outline)
+    surface_area = shoelace_formula(x_coordinates, y_coordinates)
+    return surface_area
+
+def part1(path, length):
+    area = calculate_surface_area(path)
+    total = int(area + 1 - length / 2 + length)
+    return total
+
+def part2():
+    instructions = get_instructions_part2()
+    path = get_path_part1(instructions)
+    length = get_path_length(instructions)
+    return part1(path, length)
+
+
+print('Part 1:', part1(get_path_part1(instructions), get_path_length(instructions)))
+print('Part 2:', part2())
