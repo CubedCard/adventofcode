06a252dcf78919cee78dfa087cfb26845d7e6428
Jip <78497126+CubedCard@users.noreply.github.com>
2024-12-18 09:08:05 +0100

    Improve day 18 solution

--- 2024/day-18/solution.py
+++ 2024/day-18/solution.py
@@ -4,11 +4,6 @@ corrupted = [line.strip().split(',') for line in open('data.txt')]
 
 grid = [['.' for x in range(71)] for _ in range(71)]
 
-for i, cor in enumerate(corrupted):
-    if i > 1024:
-        break
-    grid[int(cor[0])][int(cor[1])] = '#'
-
 def shortest_path(grid, start, goal):
     rows, cols = len(grid), len(grid[0])
     directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
@@ -31,23 +26,23 @@ def shortest_path(grid, start, goal):
     return -1
 
 def part_1(i=1024):
-    start = (0, 0)
-    goal = (70, 70)
+    start, goal = (0, 0), (70, 70)
 
     path_length = shortest_path(grid, start, goal)
-    if path_length != -1:
-        return path_length
-    else:
-        return i
+    return path_length if path_length != -1 else i
 
-def part_2():
+def add_corruptions(part2=False):
     for i, cor in enumerate(corrupted):
         if i > 1024:
-            valid = part_1(i)
-            if valid == i:
-                return corrupted[i - 1][0], corrupted[i - 1][1]
+            if part2:
+                if part_1(i) == i: 
+                    return ",".join((corrupted[i - 1][0], corrupted[i - 1][1]))
+            else:
+                break
         grid[int(cor[0])][int(cor[1])] = '#'
     return None
 
+add_corruptions()
+
 print(f"Part 1: {part_1()}")
-print(f"Part 2: {part_2()}")
+print(f"Part 2: {add_corruptions(True)}")
