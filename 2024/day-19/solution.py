622bf1281cd4379847d9c783eeafbb83eb9e5843
Jip <78497126+CubedCard@users.noreply.github.com>
2024-12-19 09:08:43 +0100

    Improve day 19 solution

--- 2024/day-19/solution.py
+++ 2024/day-19/solution.py
@@ -1,42 +1,36 @@
-def can_form_string(s, substrings, memo=None):
+def analyze_string(s, substrings, memo=None):
     if s == "":
-        return True
+        return (True, 1)
     if memo is None:
         memo = {}
     if s in memo:
         return memo[s]
 
-    for sub in substrings:
-        if s.startswith(sub) and can_form_string(s[len(sub):], substrings, memo):
-            memo[s] = True
-            return True
-
-    memo[s] = False
-    return False
-
-
-def count_ways_to_form_string(s, substrings, memo=None):
-    if s == "":
-        return 1
-    if memo is None:
-        memo = {}
-    if s in memo:
-        return memo[s]
-
+    can_form = False
     total_ways = 0
+
     for sub in substrings:
         if s.startswith(sub):
-            total_ways += count_ways_to_form_string(s[len(sub):], substrings, memo)
+            sub_can_form, sub_ways = analyze_string(s[len(sub):], substrings, memo)
+            if sub_can_form:
+                can_form = True
+            total_ways += sub_ways
 
-    memo[s] = total_ways
-    return total_ways
+    memo[s] = (can_form, total_ways)
+    return memo[s]
 
 data = [line.strip() for line in open("data.txt")]
 substrings = [string for string in data[0].split(", ")]
 strings = data[2:]
 
-valid_strings = [s for s in strings if can_form_string(s, substrings)]
-print(f"Part 1: {len(valid_strings)}")
+valid_strings = 0
+total = 0
 
-total = sum([count_ways_to_form_string(s, substrings) for s in strings])
-print("Part 2:", total)
+for s in strings:
+    can_form, ways = analyze_string(s, substrings)
+    if can_form:
+        valid_strings += 1
+    total += ways
+
+print(f"Part 1: {valid_strings}")
+print(f"Part 2: {total}")
