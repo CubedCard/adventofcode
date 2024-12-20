e6386337d3dfb472f5e213d2029d58dcd3ebe4cb
Jip <78497126+CubedCard@users.noreply.github.com>
2024-12-20 11:06:19 +0100

    Improve day 3 2018 solution

--- 2018/day-3/solution.py
+++ 2018/day-3/solution.py
@@ -1,27 +1,20 @@
 import re
 
-data = open('data.txt').read()
-
 grid_size = 1000
 grid = [[[] for _ in range(grid_size)] for _ in range(grid_size)]
 
-regex = r"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)"
-matches = re.findall(regex, data)
+matches = re.findall(r"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)", open('data.txt').read())
 
 for match in matches:
     n, x, y, w, h = [int(k) for k in match]
-    for i in range(w):
-        for j in range(h):
-            grid[x + i][y + j].append(n)
+    [grid[x + i][y + j].append(n) for i in range(w) for j in range(h)]
 
 def part_1(): 
     return len([1 for i in range(grid_size) for j in range(grid_size) if len(grid[i][j]) > 1])
 
 def part_2():
     dubs = [num for i in range(grid_size) for j in range(grid_size) if len(grid[i][j]) > 1 for num in grid[i][j]]
-    for x in range(1, 1349):
-        if x not in dubs:
-            return x
+    return [x for x in range(1, len(matches)) if x not in dubs][0]
 
 print(f"Part 1: {part_1()}")
 print(f"Part 2: {part_2()}")
